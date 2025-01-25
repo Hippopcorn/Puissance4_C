1cb627cb49abb08eb548bf466dad92c39eecb11b
Hippopcorn <elsa.varache@gmail.com>
2025-01-25 18:08:53 +0100

    pour les sushis

--- main.c
+++ main.c
@@ -1,56 +1,102 @@
 #include <stdio.h>  //librairie
 #include <stdlib.h> //librairie*
-#include <string.h>
 
 #define L_BOARD 7
 #define H_BOARD 6
 
-
 int nbrToken[L_BOARD] = {0, 0, 0, 0, 0, 0, 0};
-char board[L_BOARD][H_BOARD] = {' '};
+char board[L_BOARD][H_BOARD] = {
+    {' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' '}};
 
 void display();
 void printNbrToken();
+int checkWin(int x);
+int checkVertival(int x, int y);
 
-int main(int argc, char *argv[])
-{
+int main() {
+    int round = 1;
     while (1) {
         display();
         int entreeUtilisateur = 0;
-        printf("Ou voulez vous jouer votre jeton? ");
+        printf("Ou voulez vous jouer votre jeton?\n");
         scanf("%d", &entreeUtilisateur);
+
+        if (entreeUtilisateur < 1 || entreeUtilisateur > 7) {
+            printf("Entrez un numero de colonne valide svp :D\n");
+            continue;
+        }
+
         int entreeBoard = entreeUtilisateur - 1;
 
         if (nbrToken[entreeBoard] == H_BOARD) {
             printf("Cette colonne est pleine, vous ne pouvez plus mettre de jeton ici :D\n");
             continue;
         }
-        board[entreeBoard][nbrToken[entreeBoard]] = "X";
+
+        if (round % 2 == 0) {
+            board[entreeBoard][nbrToken[entreeBoard]] = 'O';
+        } else {
+            board[entreeBoard][nbrToken[entreeBoard]] = 'X';
+        }
+
+        if (checkWin(entreeBoard)) {
+            display();
+            printf("C'est gagne !\n");
+            return 0;
+        }
+
         nbrToken[entreeBoard]++;
-
-
-        printNbrToken();
-
+        round++;
+        // printNbrToken();
     }
-   
-    return 0;
 
+    return 0;
 }
 
 void display() {
-    printf("| | | | | | | |\n");
-    printf("| | | | | | | |\n");
-    printf("| | | | | | | |\n");
-    printf("| | | | | | | |\n");
-    printf("| | | | | | | |\n");
-    printf("| | | | | | | |\n");
+    printf("|%c|%c|%c|%c|%c|%c|%c|\n", board[0][5], board[1][5], board[2][5], board[3][5], board[4][5], board[5][5], board[6][5]);
+    printf("|%c|%c|%c|%c|%c|%c|%c|\n", board[0][4], board[1][4], board[2][4], board[3][4], board[4][4], board[5][4], board[6][4]);
+    printf("|%c|%c|%c|%c|%c|%c|%c|\n", board[0][3], board[1][3], board[2][3], board[3][3], board[4][3], board[5][3], board[6][3]);
+    printf("|%c|%c|%c|%c|%c|%c|%c|\n", board[0][2], board[1][2], board[2][2], board[3][2], board[4][2], board[5][2], board[6][2]);
+    printf("|%c|%c|%c|%c|%c|%c|%c|\n", board[0][1], board[1][1], board[2][1], board[3][1], board[4][1], board[5][1], board[6][1]);
+    printf("|%c|%c|%c|%c|%c|%c|%c|\n", board[0][0], board[1][0], board[2][0], board[3][0], board[4][0], board[5][0], board[6][0]);
     printf("+-+-+-+-+-+-+-+\n");
     printf(" 1 2 3 4 5 6 7 \n");
 }
 
-void printNbrToken(){
+void printNbrToken() {
     for (int i = 0; i < L_BOARD; i++) {
         printf("%d, ", nbrToken[i]);
-    } 
+    }
     printf("\n");
+}
+
+int checkWin(int x) {
+    int y = nbrToken[x];
+
+    if (checkVertival(x, y)) {
+        return 1;
+    }
+
+    return 0;
+}
+
+int checkVertival(int x, int y) {
+    if (y < 3) {
+        return 0;
+    }
+
+    for (int i = y - 1; i >= y - 3; i--) {
+        if (board[x][y] != board[x][i]) {
+            return 0;
+        }
+    }
+
+    return 1;
 }
